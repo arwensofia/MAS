61d052df60b41c4116a14b3e46dba8125b843160
YimIronfist <149625927+YimIronfist@users.noreply.github.com>
2026-03-18 14:18:41 +0200

    18_3_Jimi
    Making the minimap.

--- MAS/Assets/Scripts/UDPReciever.cs
+++ MAS/Assets/Scripts/UDPReciever.cs
@@ -3,18 +3,22 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using TMPro;
+using Unity.Collections;
 
 
 public class UDPReciever : MonoBehaviour
 {
     [Header("Network Settings")]
     public int _port = 5000; //Tablet port number
+    public float _sendRate = 0.1f; //Sends map data 10 times per second
 
     [Header("UI")]
     public TextMeshProUGUI _debugtext; //UI Text slot
+    public Transform _submarineTransform; //Submarine object goes here!
 
     private UdpClient _udpClient;
     private IPEndPoint _remoteEndPoint;
+    private float _nextSendTime;
 
     public Vector2 NetworkInput { get; private set; }
 
@@ -31,6 +35,7 @@ public class UDPReciever : MonoBehaviour
 
     private void Update()
     {
+        // RECIEVE input from the tablet
         while (_udpClient.Available > 0)
         {
             byte[] receiveBytes = _udpClient.Receive(ref _remoteEndPoint);
@@ -50,6 +55,19 @@ public class UDPReciever : MonoBehaviour
                     }
                 }
             }
+        }
+
+        // SEND position back to the tablet -> we only send if _remoteEndPoint has caught an IP from the tablet, so we know where to send it!
+        if (_remoteEndPoint.Address != IPAddress.Any && Time.time >= _nextSendTime)
+        {
+            _nextSendTime = Time.time + _sendRate;
+
+            // Format posX, posZ, rotationY
+            string posData = $"{_submarineTransform.position.x},{_submarineTransform.position.z},{_submarineTransform.eulerAngles.y}";
+            byte[] sendBytes = Encoding.UTF8.GetBytes(posData);
+
+            // Send back to the exact IP and port the tablet just used
+            _udpClient.Send(sendBytes, sendBytes.Length, _remoteEndPoint);
         }
     }
 
